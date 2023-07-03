a79080f376f408744610b3dd4c33abace1a082a2
Nathaniel Watts <reg@nathanielwatts.com>
2023-07-02 23:34:03 -0500

    Add Car, sprites, stuff

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,110 @@
-def tick args
-  args.outputs.labels  << [640, 540, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 500, 'Docs located at ./docs/docs.html and 100+ samples located under ./samples', 5, 1]
-  args.outputs.labels  << [640, 460, 'Join the Discord server! https://discord.dragonruby.org', 5, 1]
+class Car
+  SPRITE_FOLDER = "sprites/car"
+  WIDTH = 14
+  HEIGHT = 20
 
-  args.outputs.sprites << { x: 576,
-                            y: 280,
-                            w: 128,
-                            h: 101,
-                            path: 'dragonruby.png',
-                            angle: args.state.tick_count }
+  attr_reader :x, :y
+  attr_accessor :angle, :spread, :scale
 
-  args.outputs.labels  << { x: 640,
-                            y: 60,
-                            text: './mygame/app/main.rb',
-                            size_enum: 5,
-                            alignment_enum: 1 }
+  def initialize(args, scale: 6)
+    @grid = args.grid
+    @outputs = args.outputs
+
+    @x = 0
+    @y = 0
+    @scale = scale
+    @angle = 0
+    @spread = 0
+  end
+
+  def handle_input(inputs)
+    if inputs.keyboard.left
+      self.angle += 1
+    elsif inputs.keyboard.right
+      self.angle -= 1
+    end
+
+    if inputs.keyboard.up || inputs.keyboard.key_down.tab
+      self.spread += 1
+    elsif inputs.keyboard.down || inputs.keyboard.key_down.shift
+      self.spread -= 1
+    end
+
+    if inputs.keyboard.key_down.equal_sign
+      return if scale == 15
+
+      self.scale += 1
+    elsif inputs.keyboard.key_down.minus
+      return if scale == 1
+
+      self.scale -= 1
+    end
+  end
+
+  def render
+    outputs.sprites << sprites
+  end
+
+  def serialize
+    {}
+  end
+
+  private
+
+  attr_reader :grid, :outputs
+  attr_writer :x, :y
+
+  def calculate_x
+    self.x = grid.w.half - (WIDTH.half * scale)
+  end
+
+  def calculate_y
+    self.y = grid.h.half - (HEIGHT.half * scale)
+  end
+
+  def sprites
+    (0..6).to_a.map_with_index do |layer_number, index|
+      {
+        x: calculate_x,
+        y: calculate_y - (1 * spread) + (index * spread),
+        w: WIDTH * scale,
+        h: HEIGHT * scale,
+        path: "#{SPRITE_FOLDER}/img_#{layer_number}.png",
+        angle: angle,
+        source_x: 0,
+        source_w: WIDTH,
+        source_h: HEIGHT,
+      }
+    end
+  end
+end
+
+def debug(args)
+  left_padding = 30
+
+  args.outputs.labels << [left_padding, 30.from_top, "FPS: #{args.gtk.current_framerate.to_sf}"]
+  args.outputs.labels << [left_padding, 55.from_top, "X: #{args.state.car.x.to_sf}"]
+  args.outputs.labels << [left_padding, 80.from_top, "Y: #{args.state.car.y.to_sf}"]
+  args.outputs.labels << [left_padding, 105.from_top, "ANGLE: #{args.state.car.angle.to_sf}"]
+  args.outputs.labels << [left_padding, 130.from_top, "SPREAD: #{args.state.car.spread.to_sf}"]
+  args.outputs.labels << [left_padding, 155.from_top, "SCALE: #{args.state.car.scale.to_sf}"]
+end
+
+def instructions(args)
+  right_padding = args.grid.w - 330
+
+  args.outputs.labels << [right_padding, 30.from_top, "A/D || LEFT/RIGHT: Rotate".rjust(30)]
+  args.outputs.labels << [right_padding, 55.from_top, "W/S || UP/DOWN: Spread".rjust(30)]
+  args.outputs.labels << [right_padding, 80.from_top, "TAB/SHIFT: Spread (fine)".rjust(30)]
+  args.outputs.labels << [right_padding, 105.from_top, "=/-: Scale".rjust(30)]
+end
+
+def tick(args)
+  args.state.car ||= Car.new(args)
+
+  debug(args)
+  instructions(args)
+
+  args.state.car.handle_input(args.inputs)
+  args.state.car.render
 end
